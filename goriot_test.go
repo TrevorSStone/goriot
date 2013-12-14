c1ee496e3d9f45d693d62e18396e7fb803b56bcb
TrevorSStone <TrevorSStone@gmail.com>
2013-12-13 19:48:33 -0500

    Removed print statements from test file

--- goriot_test.go
+++ goriot_test.go
@@ -1,7 +1,6 @@
 package goriot
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -35,72 +34,64 @@ func TestGetLeagueBySummoner(t *testing.T) {
 
 func TestGetStatSummariesBySummoner(t *testing.T) {
 	SetAPIKey(personalkey)
-	stats, err := GetStatSummariesBySummoner(NA, 2112, SEASON3)
+	_, err := GetStatSummariesBySummoner(NA, 2112, SEASON3)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	fmt.Println(stats)
 }
 
 func TestRankedStatsBySummoner(t *testing.T) {
 	SetAPIKey(personalkey)
-	stats, err := GetRankedStatsBySummoner(NA, 2112, SEASON3)
+	_, err := GetRankedStatsBySummoner(NA, 2112, SEASON3)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	fmt.Println(stats)
 }
 
 func TestGetMasteriesBySummoner(t *testing.T) {
 	SetAPIKey(personalkey)
-	stats, err := GetMasteriesBySummoner(NA, 2112)
+	_, err := GetMasteriesBySummoner(NA, 2112)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	fmt.Println(stats)
 }
 
 func TestGetRunesBySummoner(t *testing.T) {
 	SetAPIKey(personalkey)
-	stats, err := GetRunesBySummoner(NA, 2112)
+	_, err := GetRunesBySummoner(NA, 2112)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	fmt.Println(stats)
 }
 
 func TestGetSummonerByName(t *testing.T) {
 	SetAPIKey(personalkey)
-	summoner, err := GetSummonerByName(NA, "manticorex")
+	_, err := GetSummonerByName(NA, "manticorex")
 	if err != nil {
 		t.Error(err.Error())
 	}
-	fmt.Println(summoner)
 }
 
 func TestGetSummonerByID(t *testing.T) {
 	SetAPIKey(personalkey)
-	summoner, err := GetSummonerByID(NA, 2112)
+	_, err := GetSummonerByID(NA, 2112)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	fmt.Println(summoner)
 }
 
 func TestGetSummonerNamesByID(t *testing.T) {
 	SetAPIKey(personalkey)
-	summoners, err := GetSummonerNamesByID(NA, 2112, 1111)
+	_, err := GetSummonerNamesByID(NA, 2112, 1111)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	fmt.Println(summoners)
 }
 
 func TestGetTeamBySummonerID(t *testing.T) {
 	SetAPIKey(personalkey)
-	teams, err := GetTeamBySummonerID(NA, 2112)
+	_, err := GetTeamBySummonerID(NA, 2112)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	fmt.Println(teams)
 }
